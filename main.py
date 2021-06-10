279a935305cf22b958d2dee5d485ff5bd824a9cd
maneesh.apj <maneesh.apj@hotmail.com>
2021-06-10 22:15:48 +0530

    modified script

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from os import sep
 
 input1= input("Please enter \"A\" having length 10 character : ")
 
-print("A" in input1)
-if (len(input1)==10 and ("A" in input1)):
+
+if (len(input1)==10 and (10*"A" is input1)):
    print("input string is correct")
 else:
      print("input is not in correct format")
