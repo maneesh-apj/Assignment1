961f7ce823466000d18ee6d7eb4a13e9ab99e5f4
maneesh.apj <maneesh.apj@hotmail.com>
2021-06-10 21:08:43 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from os import sep
+
+
+input1= input("Please enter string having 10 character : ")
+input2 = input("Please enter second string having 5 character : ")
+
+print(input1.upper())
+
+x=int(input("Enter first number : "))
+
+y= int(input("Enter second number : "))
+
+print("Enter", "1 to Add", "2 to Subtract", "3 to devide", sep="\n")
+
+##op=int(input("enter 1 to Add", "2 to Subtract", "3 to devide",sep="\n")) within int function it is not working
+op=int(input("Enter number : "))
+
+if op==1:
+    print(x+y)
+elif op==2:
+    print(x-y)
+elif op==3:
+    if y==0:
+        print(0)
+    else:
+        print(x/y)
+else:
+    print("Enter value is not valid")
